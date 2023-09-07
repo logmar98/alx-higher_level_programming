0d09b806a78c062c60c9884a15e6cd5567c45420
logmar98 <marwanarbib15@gmail.com>
2023-09-07 12:13:23 -0700

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
+    from sys import argv, exit
     from calculator_1 import add, sub, div, mul
     count = len(argv) - 1
     if count != 3:

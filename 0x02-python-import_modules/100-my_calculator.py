c2843d673ffd5e48e30ce0bf2f26ada26756f1b8
logmar98 <marwanarbib15@gmail.com>
2023-09-07 12:24:29 -0700

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
     from sys import argv, exit
-    from calculator_1 import add, sub, div, mul
-    count = len(argv) - 1
-    if count != 3:
+    if len(argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if argv[2] not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
     a = int(argv[1])
-    operator = argv[2]
     b = int(argv[3])
-    match operator:
-        case "+":
-            print("{} {} {} = {}".format(a, operator, b, add(a, b)))
-        case "-":
-            print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
-        case "*":
-            print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
-        case "/":
-            print("{} {} {} = {}".format(a, operator, b, div(a, b)))
-        case _:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+    print("{} {} {} = {}".format(a, argv[2], b, ops[argv[2]](a, b)))

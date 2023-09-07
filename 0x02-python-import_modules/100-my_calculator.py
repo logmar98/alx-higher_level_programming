756fd1a89fdcf41097ae967389746db1e71aaa2f
logmar98 <marwanarbib15@gmail.com>
2023-09-07 12:20:21 -0700

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -11,13 +11,13 @@ if __name__ == "__main__":
     b = int(argv[3])
     match operator:
         case "+":
-            print("{} + {} = {}".format(a, b, add(a, b)))
+            print("{} {} {} = {}".format(a, operator, b, add(a, b)))
         case "-":
-            print("{} - {} = {}".format(a, b, sub(a, b)))
+            print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
         case "*":
-            print("{} * {} = {}".format(a, b, mul(a, b)))
+            print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
         case "/":
-            print("{} / {} = {}".format(a, b, div(a, b)))
+            print("{} {} {} = {}".format(a, operator, b, div(a, b)))
         case _:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)

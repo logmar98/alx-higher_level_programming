7334faa2a02289debe058199874ca3aa3f9d4713
logmar98 <marwanarbib15@gmail.com>
2023-09-07 08:08:30 -0700

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) == 0:
+        chr = "."
+    else:
+        chr = ":"
+    print("{} argument{}".format(int(len(argv)) - 1, chr), )
+    for i in range(1, int(len(argv))):
+        print("{}: {}".format(i, argv[i]))

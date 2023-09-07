39e7816c8e5cc8d3a1a14ca092dbd9371644fc65
logmar98 <marwanarbib15@gmail.com>
2023-09-07 12:01:27 -0700

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    from add_0 import add, sub, div, mul
+    from calculator_1 import add, sub, div, mul
     count = len(argv) - 1
     if count != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")

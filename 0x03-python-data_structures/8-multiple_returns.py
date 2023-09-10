78d76630a28988b01b6dcc1aaf7f1b9b61d1d1e0
logmar98 <marwanarbib15@gmail.com>
2023-09-10 08:58:35 -0700

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    return int(len(sentence)), sentence[0]
+    if len(sentence) == 0:
+        first = 'None'
+    else:
+        first = sentence[0]
+    return len(sentence), first

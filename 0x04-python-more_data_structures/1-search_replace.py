19175571405483dad749da1fea912fd40ec0faf9
logmar98 <marwanarbib15@gmail.com>
2023-09-13 17:48:02 -0700

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,5 +3,5 @@ def search_replace(my_list, search, replace):
     new_list = my_list.copy()
     for i in range(len(my_list)):
         if my_list[i] == search:
-            my_list[i] = replace
+            new_list[i] = replace
     return new_list

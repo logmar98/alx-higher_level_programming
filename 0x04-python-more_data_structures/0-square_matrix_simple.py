99fce58ad8003524cf293271a4bc849002db7583
logmar98 <marwanarbib15@gmail.com>
2023-09-13 17:30:35 -0700

    0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for i in matrix:
+        new_matrix.append(list(map(lambda x: x ** 2, i)))
+    return new_matrixdef square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for i in matrix:
+        new_matrix.append(list(map(lambda x: x ** 2, i)))
+    return new_matrix

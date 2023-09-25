a607935b916daed73b7e7ec656adb988c81a4d2e
logmar98 <marwanarbib15@gmail.com>
2023-09-25 11:22:26 -0700

    1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    """Print integer.
-
-    Args:
-        value: int
-
-    Returns:
-        true or false
-    """
     try:
         print("{:d}".format(value))
         return True

a341d6c757a8283693212d1c5c00165a4d8af509
logmar98 <marwanarbib15@gmail.com>
2023-09-26 11:31:46 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""this our class Square."""
+class Square:
+    """it just a class that skip Square."""
+    def __init__(self, size):
+        """make new square
+
+        Args:
+            size (int): is a size 
+        """
+        self.__size = size
+

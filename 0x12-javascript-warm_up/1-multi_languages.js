2b5a59892bdcc6b690f52140e679d8a4b0339e2c
logmar98 <marwanarbib15@gmail.com>
2024-01-08 22:32:06 +0100

    update task 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,6 +2,6 @@
 const C = 'C is fun';
 const Python = 'Python is cool';
 const js = 'JavaScript is amazing';
-console.log(C);
-console.log(Python);
-console.log(js);
+console.log(String(C));
+console.log(String(Python));
+console.log(String(js));

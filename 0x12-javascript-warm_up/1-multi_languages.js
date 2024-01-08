2ad4c42683910bacce7f847670152b6a4f52f262
logmar98 <marwanarbib15@gmail.com>
2024-01-08 18:54:42 +0100

    add task 0 1 2 3 4

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+let C = "C is fun";
+let Python = "Python is cool";
+let js = "JavaScript is amazing";
+console.log(C);
+console.log(Python);
+console.log(js);
---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+let  args = process.argv[2];
+if (args) {
+    console.log(args);
+} else {
+    console.log("No argument")
+}

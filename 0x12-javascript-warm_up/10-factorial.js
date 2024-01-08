ed5f1a36b10c44f0e0e4f2a7e5c9380ebc70bd21
logmar98 <marwanarbib15@gmail.com>
2024-01-08 22:52:10 +0100

    update tasks

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,7 +3,7 @@ const arg1 = process.argv[2];
 const num = Math.floor(Number(arg1));
 
 function factorial (num) {
-  if (num === 0) {
+  if (num === 0 || isNaN(num)) {
     return 1;
   } else {
     return num * factorial(num - 1);

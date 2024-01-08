e2ec57d5801a8fc2874f968f82641459b797c433
logmar98 <marwanarbib15@gmail.com>
2024-01-08 22:57:47 +0100

    update task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const arr = process.argv.slice(2);
 const sortedArr = arr.sort((a, b) => b - a);
-if (arr.length == 0) {
+if (arr.length === 0) {
   console.log(0);
-} else if (arr.length == 1) {
+} else if (arr.length === 1) {
   console.log(0);
 } else {
   console.log(Number(sortedArr[1]));

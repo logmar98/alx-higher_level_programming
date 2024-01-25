1131305f9f76e5ca2ce07da34dfd848321ad2ac7
logmar98 <marwanarbib15@gmail.com>
2024-01-25 18:58:22 +0100

    update files

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,8 +4,8 @@
 
 def find_peak(list_of_integers):
     """Finds a hightest number"""
+    
     if list_of_integers is None or list_of_integers == []:
         return None
     sorted_list = sorted(list_of_integers)
     return sorted_list[-1]
-

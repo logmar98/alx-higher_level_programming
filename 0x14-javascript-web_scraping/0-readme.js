c1e7601dab4ba3e68d19dddf643a4153b69417c2
logmar98 <marwanarbib15@gmail.com>
2024-02-20 18:39:28 +0100

    update task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+function printContent(filePath) {
+    try {
+        const content = fs.readFileSync(filePath, 'utf-8');
+        console.log(content);
+    } catch (err) {
+        console.error(err);
+    }
+}
+const filePath = process.argv[2];
+
+
+printContent(filePath);
+

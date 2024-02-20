72674f83e63c7ba7b30e79d62781e6b9649aa8f1
logmar98 <marwanarbib15@gmail.com>
2024-02-20 19:25:11 +0100

    update task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,18 @@
+const request = require('request');
+
+const apiUrl = process.argv[2];
+const charId = '18';
+
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const filmsData = JSON.parse(body);
+
+    const actorFilms = filmsData.results.filter(film =>
+      film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${charId}/`)
+    );
+
+    console.log(actorFilms.length);
+  }
+});

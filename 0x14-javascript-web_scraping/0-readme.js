793b2d27346df122d51c7fd8a103204ba44493fc
logmar98 <marwanarbib15@gmail.com>
2024-02-20 18:44:13 +0100

    update task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,15 +1,14 @@
+#!/usr/bin/node
 const fs = require('fs');
 
-function printContent(filePath) {
-    try {
-        const content = fs.readFileSync(filePath, 'utf-8');
-        console.log(content);
-    } catch (err) {
-        console.error(err);
-    }
+function printContent (filePath) {
+  try {
+    const content = fs.readFileSync(filePath, 'utf-8');
+    console.log(content);
+  } catch (err) {
+    console.error(err);
+  }
 }
 const filePath = process.argv[2];
 
-
 printContent(filePath);
-
